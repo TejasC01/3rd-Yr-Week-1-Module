3846a2379463fdd3334422e1473e503caf9e587b
Tejas Kumar <tejaskumar1005@gmail.com>
2026-06-13 09:28:12 +0530

    S_4

---
+++ ques/S_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 5; i >= 1; i--)
+    {
+        for(int s=0;s<5-i;s++){
+            cout<<" ";
+        }
+
+
+        for (int j = 1; j <=i; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
