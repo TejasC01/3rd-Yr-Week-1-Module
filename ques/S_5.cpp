e465365458cbb44f5ffa472ac12cc9e1d4bdab10
Tejas Kumar <tejaskumar1005@gmail.com>
2026-06-30 10:29:44 +0530

    modify file

--- ques/S_5.cpp
+++ ques/S_5.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-
-    for (int i = 0; i <5; i++) {
-        
-        for (int j = 0; j <5-i; j++) {
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    for (int i = 0; i < n ; i++)
+    {
+        for (int s = 0; s < n -i-1; s++)
+        {
             cout << " ";
         }
 
-        
-        for (int j = 1; j <= (2 * i - 1); j++) {
+        for (int j = 1; j <= 2 * i + 1; j++)
+        {
             cout << "*";
         }
-
-        cout << endl;
+        for (int s = 0; s < n -i- 1; s++)
+        {
+            cout << " ";
+        }
+        cout << endl ;
     }
-
     return 0;
 }
