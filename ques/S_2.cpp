6470fae88e1e229a5b7e498cce497ebd8804d140
Tejas Kumar <tejaskumar1005@gmail.com>
2026-07-18 02:17:06 +0530

    Solution fix

--- ques/S_2.cpp
+++ ques/S_2.cpp
@@ -1,33 +1,16 @@
 #include <iostream>
 using namespace std;
-int main()
-{
 
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "*";
-    }
-    cout << "\n";
+int main() {
 
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            if (j == 0 || j == 4)
-            {
+    for (int i = 0; i <5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 0 || i == 4 || j == 0 || j == 4)
                 cout << "*";
-            }
             else
-            {
                 cout << " ";
-            }
         }
-        cout << "\n";
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "*";
+        cout << endl;
     }
 
     return 0;
--- ques/S_3.cpp
+++ ques/S_3.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main()
 {
-    for (int i = 1; i < 6; i++)
+    for (int i = 0; i < 5; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 0; j <= i; j++)
         {
             cout << "*";
         }
-        cout << "\n";
+        cout <<endl;
     }
 
     return 0;
